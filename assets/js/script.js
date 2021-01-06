760583109145e2599b34b004d02a59029c3ac68f
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-01-06 14:37:09 -0700

    edit tasks complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,12 +35,9 @@ var loadTasks = function() {
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
     // then loop over sub-array
-    // arr.forEach(function(task) {
-    //  createTask(task.text, task.date, list);
-    // });
-    for (var i = 0, len = arr.length; i < len; i++) {
-      createTask(arr[i]);
-      }
+    arr.forEach(function(task) {
+     createTask(task.text, task.date, list);
+    });
   });
 };
 
@@ -114,6 +111,37 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 
+// value of due date was changed
+$(".list-group").on("blur","input[type='text']", function (){
+  //get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
